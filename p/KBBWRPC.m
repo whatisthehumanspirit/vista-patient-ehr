de9f8538f0c4991681e76cf618c387e9513a11d4
Alexis Carlson <whatisthehumanspirit@gmail.com>
2016-03-24 10:30:05 -0700

    Start unit tests.

--- p/KBBWRPC.m
+++ p/KBBWRPC.m
@@ -1,12 +1,26 @@
-KBBWRPC;VEN/ARC - PATIENT EHR RPC;11/5/2015
+KBBWRPC ; VEN/ARC - PATIENT EHR RPC ; 11/5/2015
  ;;1.0;KBBW PEHR;**LOCAL**;NOV 5, 2015
  ;
+ I $T(EN^%ut)'="" D EN^%ut("KBBWRPC",2)
+ Q
+ ;
+STARTUP ; Runs once per routine
+ Q
+ ;
+SETUP ; Runs once per test
+ Q
+ ;
+TEARDOWN ; Runs once per test
+ Q
+ ;
+SHUTDOWN ; Runs once per routine. Probably won't use this.
  Q
  ;
 STATUS() ; Is the PEHR enabled?
  Q $$GET^XPAR("PKG","KBBW PEHR ENABLE",1,"Q")
  ;
 PATIENT(PT) ; RPC call for basic user/patient info [RPC broker call name]
+ ;            So pass parameter by reference
  K PT
  Q:'DUZ
  S PT=DUZ_U_$P($G(^VA(200,DUZ,0)),U)_U
