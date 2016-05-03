8562cf8c55ec8addc1388a2ceb587a7e543c85f9
Alexis Carlson <whatisthehumanspirit@gmail.com>
2016-05-03 12:40:06 -0700

    Update comments on formal comments.

--- r/KbbwPehrRpc.m
+++ r/KbbwPehrRpc.m
@@ -53,6 +53,7 @@ PehrEnabled() ; Is the PEHR enabled?
  ;
 UserSettings(info) ; UserInfo(.info)
  ;ven/arc;test;pseudo-function;messy;silent;non-sac;non-recursive
+ ;ven/arc;test/production;procedure/pseudo-function/function;clean/messy;silent/report/dialogue;recursive/non-sac;non-recursive
  ;
  ; RPC call for basic user/patient info
  ;
